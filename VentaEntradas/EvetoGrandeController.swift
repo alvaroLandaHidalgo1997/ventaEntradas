5619db2258a44396b869f8486e8d3d4da96e6fea
alumnos <alumnos@409-15.local>
2019-01-11 13:41:12 +0100

    pantallas definitivas y tabla view hace match

---
+++ VentaEntradas/EvetoGrandeController.swift
@@ -0,0 +1,12 @@
+//
+//  EvetoGrandeController.swift
+//  VentaEntradas
+//
+//  Created by alumnos on 12/12/18.
+//  Copyright © 2018 Alvaro. All rights reserved.
+//
+
+import UIKit
+class EventoGrande{
+    
+}
---
+++ VentaEntradas/MisEntradasController.swift
@@ -0,0 +1,12 @@
+//
+//  MisEntradasController.swift
+//  VentaEntradas
+//
+//  Created by alumnos on 12/12/18.
+//  Copyright © 2018 Alvaro. All rights reserved.
+//
+
+import  UIKit
+class MisEntradasController{
+    
+}
