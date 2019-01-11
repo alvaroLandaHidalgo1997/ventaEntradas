052dd9ab0e3db4d2aee786d627834e5901b5830e
alumnos <alumnos@409-15.local>
2019-01-11 14:31:42 +0100

    tableView avanzado, completar con enlace

--- VentaEntradas/ViewController.swift
+++ VentaEntradas/ViewController.swift
@@ -3,6 +3,11 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var fotoseventos = [UIImage]();
+    var foto1 = UIImage(named: "black veil brides")
+    var foto2 = UIImage(named: "Japan weekend")
+    var foto3 = UIImage(named: "red hot chilly peppers")
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -11,4 +16,14 @@ class ViewController: UITableViewController {
         super.didReceiveMemoryWarning()
 
     }
+    func añadirfotos(){
+        fotoseventos = [foto1, foto2, foto3] as! [UIImage];
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fotoseventos.count
+    }
+    //https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/CreateATableView.html#//apple_ref/doc/uid/TP40015214-CH8-SW1
 }
