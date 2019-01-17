b14d317b2c3612a3998c348bb29811c7466f37a4
Alvaro Landa <alvarolanda@MacBook-Pro-de-Alvaro.local>
2019-01-17 22:07:37 +0100

    evento grande debería rular, pero no responde

--- VentaEntradas/EvetoGrandeController.swift
+++ VentaEntradas/EvetoGrandeController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 
 class EventoGrande: UIViewController{
+    var nombreGrupo: String = "";
+    var imagenGrupo:UIImageView? ;
     
-
     @IBOutlet weak var imagengrupo: UIImageView!
     
-    @IBOutlet weak var QRImage: UIImageView!
     
     @IBOutlet weak var nombregrupo: UILabel!
     
+    init(nombregrupo: String, imagengrupo: UIImageView){
+        super.init(nibName: nil, bundle:nil)
+        self.nombregrupo.text = nombregrupo;
+        self.imagengrupo = imagengrupo;
+    }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
